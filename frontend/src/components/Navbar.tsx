fa60d8e0559ae9b953771ac92735ef93e71c4948
Kyaw Zin Thant <kyawzinucsm89@gmail.com>
2025-06-24 11:01:17 +0700

    update Home, Projects & Contact

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,13 +4,13 @@ import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
 export default function Navbar() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
+  // const { theme, setTheme } = useTheme();
+  // const [mounted, setMounted] = useState(false);
 
-  useEffect(() => setMounted(true), []);
-  useEffect(() => {
-    console.log('Current theme:', theme);
-  }, [theme]);
+  // useEffect(() => setMounted(true), []);
+  // useEffect(() => {
+  //   console.log('Current theme:', theme);
+  // }, [theme]);
   return (
     <nav className="w-full py-4 px-4 sm:px-6 md:px-8 bg-white text-black dark:bg-gray-800 dark:text-white shadow-md flex flex-col sm:flex-row justify-between items-center gap-4 sticky top-0 z-50">
       <h1 className="font-bold text-xl">Kyaw Zin Thant</h1>
